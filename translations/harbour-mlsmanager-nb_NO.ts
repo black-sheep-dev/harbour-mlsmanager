7089c4c8ec9b0e1bc9dc7935e1804478669266a5
Allan Nordhøy <epost@anotheragency.no>
2021-02-11 09:56:30 +0100

    Added translation using Weblate (Norwegian Bokmål)

---
+++ translations/harbour-mlsmanager-nb_NO.ts
@@ -0,0 +1,140 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MLS Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Manager for Mozilla Location Service offline packages for Sailfish OS.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sources</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Donations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If you like my work you can buy me a beer.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hide search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Offline Packages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Install</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No offline packages available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Check your internet connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Update all packages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Updating all packages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Installing offline package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Removing offline package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Updating offline package</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PackageManager</name>
+    <message>
+        <source>Failed to refresh repository</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to remove package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to install package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to update package(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Package installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Package removed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Package(s) updated</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>harbour-mlsmanager</name>
+    <message>
+        <source>Updates available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Click to show updates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Updates of MLS offline packages are available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
