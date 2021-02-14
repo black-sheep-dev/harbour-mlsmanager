3ddff2707b591a7c80068c9d4528c00399569fa3
Ladislav Hodas <ladislav@hodas.sk>
2021-02-14 07:04:33 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (33 of 33 strings)
    
    Translation: harbour-mlsmanager/harbour-mlsmanager
    Translate-URL: https://hosted.weblate.org/projects/harbour-mlsmanager/harbour-mlsmanager/sk/

--- translations/harbour-mlsmanager-sk.ts
+++ translations/harbour-mlsmanager-sk.ts
@@ -5,136 +5,136 @@
     <name>AboutPage</name>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>O aplikácii</translation>
     </message>
     <message>
         <source>MLS Manager</source>
-        <translation type="unfinished"></translation>
+        <translation>MLS Manager</translation>
     </message>
     <message>
         <source>Manager for Mozilla Location Service offline packages for Sailfish OS.</source>
-        <translation type="unfinished"></translation>
+        <translation>Správca pre polohovaciu službu Mozilla bez pripojenia pre Sailfish OS.</translation>
     </message>
     <message>
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Zdroje</translation>
     </message>
     <message>
         <source>Donations</source>
-        <translation type="unfinished"></translation>
+        <translation>Darovania</translation>
     </message>
     <message>
         <source>If you like my work you can buy me a beer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ak sa Vám páči moja práca, môžete mi kúpiť pivo.</translation>
     </message>
 </context>
 <context>
     <name>MainPage</name>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>O aplikácii</translation>
     </message>
     <message>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>Obnoviť</translation>
     </message>
     <message>
         <source>Hide search</source>
-        <translation type="unfinished"></translation>
+        <translation>Skryť hľadanie</translation>
     </message>
     <message>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Hľadať</translation>
     </message>
     <message>
         <source>Offline Packages</source>
-        <translation type="unfinished"></translation>
+        <translation>Uložené balíky</translation>
     </message>
     <message>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>Inštalovať</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Odstrániť</translation>
     </message>
     <message>
         <source>No offline packages available</source>
-        <translation type="unfinished"></translation>
+        <translation>Žiadne uložené balíky</translation>
     </message>
     <message>
         <source>Check your internet connection</source>
-        <translation type="unfinished"></translation>
+        <translation>Skontrolujte internetové pripojenie</translation>
     </message>
     <message>
         <source>Update</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktualizovať</translation>
     </message>
     <message>
         <source>Update all packages</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktualizovať všetky balíky</translation>
     </message>
     <message>
         <source>Updating all packages</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktualizácia všetkých balíkov</translation>
     </message>
     <message>
         <source>Installing offline package</source>
-        <translation type="unfinished"></translation>
+        <translation>Inštalovanie uložených balíkov</translation>
     </message>
     <message>
         <source>Removing offline package</source>
-        <translation type="unfinished"></translation>
+        <translation>Odstraňovanie uložených balíkov</translation>
     </message>
     <message>
         <source>Updating offline package</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktualizácia uložených balíkov</translation>
     </message>
 </context>
 <context>
     <name>PackageManager</name>
     <message>
         <source>Failed to refresh repository</source>
-        <translation type="unfinished"></translation>
+        <translation>Obnovenie úložiska zlyhalo</translation>
     </message>
     <message>
         <source>Failed to remove package</source>
-        <translation type="unfinished"></translation>
+        <translation>Odstránenie balíka zlyhalo</translation>
     </message>
     <message>
         <source>Failed to install package</source>
-        <translation type="unfinished"></translation>
+        <translation>Inštalovanie balíka zlyhalo</translation>
     </message>
     <message>
         <source>Failed to update package(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktualizácia balíka(ov) zlyhala</translation>
     </message>
     <message>
         <source>Package installed</source>
-        <translation type="unfinished"></translation>
+        <translation>Balík je inštalovaný</translation>
     </message>
     <message>
         <source>Package removed</source>
-        <translation type="unfinished"></translation>
+        <translation>Balík je odstránený</translation>
     </message>
     <message>
         <source>Package(s) updated</source>
-        <translation type="unfinished"></translation>
+        <translation>Balík(y) sú aktualizované</translation>
     </message>
 </context>
 <context>
     <name>harbour-mlsmanager</name>
     <message>
         <source>Updates available</source>
-        <translation type="unfinished"></translation>
+        <translation>Je dostupná aktualizácia</translation>
     </message>
     <message>
         <source>Click to show updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Klepnúť a pre zobrazenie aktualizácií</translation>
     </message>
     <message>
         <source>Updates of MLS offline packages are available.</source>
-        <translation type="unfinished"></translation>
+        <translation>Je dostupná aktualizácia uložených MLS balíkov.</translation>
     </message>
 </context>
 </TS>
