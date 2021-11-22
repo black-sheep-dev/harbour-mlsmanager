52fe2ac65c8a32f2168ab730a1367874f0904777
Weblate Admin <blacksheep@nubecula.org>
2021-11-22 19:29:50 +0000

    Added translation using Weblate (French)

---
+++ translations/harbour-mlsmanager-fr.ts
@@ -0,0 +1,219 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MLS Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Manager for Mozilla Location Service offline packages for Sailfish OS.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sources</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Donations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If you like my work why not buy me a beer?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Donate with PayPal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Donate with Liberpay</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Give star</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hide search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Install</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No offline packages available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Check your internet connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Update all packages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Updating all packages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Installing offline package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Removing offline package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Updating offline package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Countries</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PackageManager</name>
+    <message>
+        <source>Failed to refresh repository</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to remove package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to install package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to update package(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Package installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Package removed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Package(s) updated</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RegionsPage</name>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Update all packages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Updating all packages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Regions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No offline packages available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Check your internet connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Install</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Installing offline package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Removing offline package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Updating offline package</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>harbour-mlsmanager</name>
+    <message>
+        <source>Updates available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Click to show updates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Updates of MLS offline packages are available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
